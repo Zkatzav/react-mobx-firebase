a4e9bffa13ea51e466ea01698555b7be37a8c2d2
ziv katzav <zkatzav94@gmail.com>
2019-09-23 14:17:09 +0300

    firebase signup

---
+++ src/components/auth/PassChange.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PassChange() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PassChange
---
+++ src/components/auth/PassForget.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PassForget() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PassForget
