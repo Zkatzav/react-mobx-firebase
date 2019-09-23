4460d87ecf7d0fbfb929ebedde0d95b97fdee057
ziv katzav <zkatzav94@gmail.com>
2019-09-23 12:09:41 +0300

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      app
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navigation from './components/Navigation';
+import LandingPage from './components/Landing';
+import SignUpPage from './components/auth/SignUp';
+import SignInPage from './components/auth/SignIn';
+import PasswordForgetPage from './components/auth/PassForget';
+import HomePage from './components/Home';
+import AccountPage from './components/Account';
+import AdminPage from './components/Admin';
+import * as ROUTES from './constants/routes';
+
+
+const App = () => (
+  <Router>
+    <div>
+      <Navigation />
+
+      <hr />
+
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
     </div>
-  );
-}
+  </Router>
+);
+
 
 export default App;
